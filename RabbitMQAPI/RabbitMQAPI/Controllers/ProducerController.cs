10f540c876c2fdb221e6933bc8e83db0a11da14a
vamshi408 <bandi.vamshi408@gmail.com>
2023-12-17 21:27:48 +0530

    RabbitMQ Code

---
+++ RabbitMQAPI/RabbitMQAPI/Controllers/ProducerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQAPI.Models;
+using System.Text;
+
+namespace RabbitMQAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        [HttpPost("SendMessageToRabbitMQ")]
+        public void SendMessageToRabbitMQ(MessageModel messagemodel)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "vamshiQueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "ID:" + messagemodel.Id + ", DateTime:" + messagemodel.DateTime + ", Message:" + messagemodel.Message;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "vamshiQueue",
+                        body: body);
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQAPI/RabbitMQAPI/Models/MessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQAPI.Models
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+        public required string Message { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
